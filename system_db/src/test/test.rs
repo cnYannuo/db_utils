1a4c001e8545dd1cbf21abe9e2b5fdd9f72433bc
cnYannuo <642410979@qq.com>
2023-03-02 15:35:34 +0800

    提交测试文件+一些描述

--- system_db/src/test/test.rs
+++ system_db/src/test/test.rs
@@ -1,14 +1,5 @@
-# db_utils
-## 主要是为mysql做的一些简化curd操作，集成了许多工具的功能
-
-## db的连接在对应配置里面
-### application.toml为第一层的配置文件
-### dev为生产环境文件,test为测试环境文件  里面集成了db连接方式
-
-## 最重要的是macro_builder中的过程宏，在quote!块里面修改或新增功能，并且在CommonDbImpl trait模块里面添加对应函数
-
-### 例子：
- ```
+#[cfg(test)]
+mod test {
     use macro_builder::CommonDbMacro;
     use serde_derive::{Serialize, Deserialize};
 
@@ -29,20 +20,15 @@
 
     #[test]
     fn test() {
-        // 获取表数据的数量
         let number = User::get_count("table");
         println!("number: {}", number);
-        // 获取表数据
         let user_list = User::batch_select("table", 1, 100);
         for item in user_list {
             println!("user: {:?}", item);
         }
         let user = User::new(1, "tester".to_owned(), "earth".to_owned(), 10086);
-        // 插入
         user.insert("table");
-        // 更新
         user.update("table");
-        // 删除
         User::delete("table", 1);
     }
- ```
+}
